7fa7f6c9ad7f2ea27a85bff2f9668c2cdc4a8dd2
Ivan Cardozo <ivancardozo11@hotmail.com>
2024-01-17 05:30:27 +0100

    remove unused endpoint

--- src/services/jobService.js
+++ src/services/jobService.js
@@ -32,30 +32,5 @@ const getUnpaidJobsForUser = async (profileId) => {
         throw new Error(error.message);
     }
 };
-const getAllUnpaidJobsForUser = async (profileId) => {
-    try {
-        const activeContracts = await Contract.findAll({
-            where: {
-                [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }],
-                status: 'in_progress'
-            },
-            attributes: ['id']
-        });
 
-        const activeContractIds = activeContracts.map(contract => contract.id);
-
-        const unpaidJobs = await Job.findAll({
-            where: {
-                ContractId: { [Op.in]: activeContractIds },
-                paid: false
-            }
-        });
-
-        return unpaidJobs;
-    } catch (error) {
-        console.error('Error in getUnpaidJobsForActiveContracts:', error);
-        throw new Error(error.message);
-    }
-};
-
-export { getUnpaidJobsForUser, getAllUnpaidJobsForUser };
+export { getUnpaidJobsForUser };
